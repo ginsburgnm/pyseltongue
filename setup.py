2ac41faebd53c3834e018118937d57337f43a9dc
nginsburg <ginsburgnm@ornl.gov>
2019-09-24 14:35:46 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+"""
+Secret Sharing
+==============
+
+"""
+
+from setuptools import setup
+
+setup(
+    name='nginsecretsharing',
+    version='0.3.0',
+    url='https://github.com/ginsburgnm/secret-sharing',
+    license='MIT',
+    author='nginsburg',
+    author_email='ginsburgnm@gmail.com',
+    description=("Tools for sharing secrets (like Bitcoin private keys), "
+                 "using shamir's secret sharing scheme."),
+    packages=[
+        'nginsecretsharing',
+    ],
+    zip_safe=False,
+    install_requires=[
+        'six',
+        'utilitybelt',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
